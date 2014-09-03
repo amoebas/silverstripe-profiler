87da636324c3fbf4615cd195d282a3870b3fb0fa
Stig Lindqvist <stojg.lindqvist@gmail.com>
2014-09-04 09:41:13 +1200

    Remove doubleclick and scrolling

--- javascript/dbprofiler.js
+++ javascript/dbprofiler.js
@@ -1,17 +1,12 @@
 $(document).ready(function() {
 	
 	$('td .cursor').each(function(){
-		$(this).dblclick(function(){
+		$(this).click(function(){
 			var hidden = $(this).parent().find('.hide');
 			var displayed = $(this).parent().find('.display');
 			console.log(displayed);
 			displayed.removeClass('display').addClass('hide');
 			hidden.removeClass('hide').addClass('display');
-
-			$('html, body').animate({
-				scrollTop: $(this).parent().offset().top
-			}, 0);
-
 			return false;
 		});
 	});
